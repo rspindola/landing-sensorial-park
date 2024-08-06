724d27f054e5d5ad65497fb411616de89a0de874
Renato Castro <rspindola@live.com>
2024-08-06 19:12:40 -0300

    feat: add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'design-gray': '#767c75',
+        'black': '#1e2326',
+        'yellow-woods': '#b8af45',
+        'input': '#848983'
+      },
+      height: {
+        '128': '34rem',
+        '9/100': '90%'
+      },
       letterSpacing: {
         default: '.25em',
       }
@@ -12,4 +22,3 @@ module.exports = {
     require('@tailwindcss/forms')
   ],
 }
-
